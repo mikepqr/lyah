05264c6ab94fe87b00e38e5b11187767de14ea83
Mike Lee Williams <mike@mike.place>
2018-07-18 17:11:47 -0700

    Add rest of notes on Chapter 5: Higher order functions

--- baby.hs
+++ baby.hs
@@ -127,3 +127,6 @@ sum' xs = foldl f 0 xs
 sqrtSums :: Int
 sqrtSums = length (takeWhile (<1000) (scanl1 (+) (map sqrt [1..]))) + 1
 
+oddSquareSum :: Integer
+oddSquareSum = sum (takeWhile (<10000) (filter odd (map (^2) [1..])))
+oddSquareSum' = (sum . takeWhile (<10000) . filter odd . map (^2))
