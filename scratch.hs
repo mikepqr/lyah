f80ef501ae06199891290ab63575d8a2659f23a7
Mike Lee Williams <mike@mike.place>
2018-08-25 12:48:46 -0700

    Add notes on Chapter 7: Types and Type Classes

--- scratch.hs
+++ scratch.hs
@@ -155,3 +155,18 @@ data Person = Person { firstName :: String
                      } deriving (Eq, Show, Read)
 
 data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show)
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+    | x == a = Node x left right
+    | x < a  = Node a (treeInsert x left) right
+    | x > a  = Node a left (treeInsert x right)
+instance Functor Tree where
+    fmap f EmptyTree = EmptyTree
+    fmap f (Node x left right) = Node (f x) (fmap f left) (fmap f right)
+
+reunmap f m = Map.fromList . map (\(key, val) -> (key, f val)) $ Map.toList m
